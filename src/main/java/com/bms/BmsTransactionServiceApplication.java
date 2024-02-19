ba19ecbb0d20a9f69c35e32da8d4332290300e12
vishal-cqs <vishal@cqs.in>
2024-02-19 17:12:52 +0530

    initializing files

---
+++ src/main/java/com/bms/BmsTransactionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmsTransactionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmsTransactionServiceApplication.class, args);
+	}
+
+}
